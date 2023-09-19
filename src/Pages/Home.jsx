ab481b2c8700cdaec1c70851de1139762d80d8a8
Aymerick Toonba <godzilla.toonba@gmail.com>
2023-09-19 13:27:52 +0200

    Initialization of the app with 2 routes '/' and '/transactions'

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+
+
+function Home(){
+  return (
+    <div>Hello World Home Page</div>
+  )
+}
+
+export default Home
---
+++ src/Pages/Transaction.jsx
@@ -0,0 +1,9 @@
+
+
+function Transactions(){
+  return (
+    <div>Hello World Transaction Page</div>
+  )
+}
+
+export default Transactions
